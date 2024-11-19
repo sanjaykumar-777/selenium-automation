21a9561a0f14ff800edb5345f1b3a2df2f84b1c8
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-11-19 23:43:35 +0530

    implemented base class for test and first test method

--- src/main/java/com/saucedemo/pages/LoginPage.java
+++ src/main/java/com/saucedemo/pages/LoginPage.java
@@ -8,7 +8,7 @@ public class LoginPage extends BasePage {
     private By usernameField = By.id("user-name");
     private By passwordField = By.id("password");
     private By loginButton = By.id("login-button");
-    private By errorMessage = By.xpath("#login_button_container h3");
+    private By errorMessage = By.cssSelector("#login_button_container h3");
 
     //methods or action on login page
     //1.enter the username - standard_user
