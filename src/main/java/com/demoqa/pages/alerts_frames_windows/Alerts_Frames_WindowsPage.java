fe3afaf12325eb47721d04269698f479d9a93c4c
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-12-15 20:08:02 +0530

    switch to alerts

--- src/main/java/com/demoqa/pages/alerts_frames_windows/Alerts_Frames_WindowsPage.java
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/Alerts_Frames_WindowsPage.java
@@ -8,9 +8,15 @@ import static utilities.JavaScriptUtility.scrollToElementJS;
 public class Alerts_Frames_WindowsPage extends HomePage {
 
     private By modalDialogsMenuItem = By.xpath("//li[@id='item-4']/span[text()='Modal Dialogs']");
+    private By alertsMenuItem = By.xpath("//li[@id='item-1']/span[text()='Alerts']");
     public ModalDialogsPage clickModalDialogs() {
         scrollToElementJS(modalDialogsMenuItem);
         click(modalDialogsMenuItem);
         return new ModalDialogsPage();
+    }
+    public AlertsPage clickAlerts() {
+        scrollToElementJS(alertsMenuItem);
+        click(alertsMenuItem);
+        return new AlertsPage();
     }
 }
---
+++ src/test/java/com/part3_4/demoqa/tests/part4/alerts/AlertsTest.java
@@ -0,0 +1,43 @@
+package com.part3_4.demoqa.tests.part4.alerts;
+
+import com.part3_4.demoqa.base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static utilities.SwitchToUtility.*;
+
+@Test
+public class AlertsTest extends BaseTest {
+    public void testInformationAlert() {
+        String expectedAlertText = "You clicked a button";
+        var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickInformationAlertButton();
+
+        Assert.assertEquals(getAlertText(), expectedAlertText,
+                "\n Actual & Expected Messages Do Not Match \n");
+        acceptAlert();
+    }
+
+    public void testConfirmationAlert() {
+        var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickConfirmationAlertButton();
+        dismissAlert();
+        String actualConfirmationResult = alertsPage.getConfirmationResult();
+        String expectedConfirmationResult = "You selected Cancel";
+        Assert.assertEquals(actualConfirmationResult, expectedConfirmationResult,
+                "\n You Did Not Select Cancel \n");
+    }
+
+    public void testPromptAlert() {
+        String alertText = "Selenium With Java";
+        String expectedResult = "You entered " + alertText;
+
+        var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickPromptAlertButton();
+        setAlertText(alertText);
+        acceptAlert();
+        String actualResult = alertsPage.getPromptAlertResult();
+        Assert.assertEquals(actualResult, expectedResult,
+                "\n Actual & Expected Results Do Not Match \n");
+    }
+}
